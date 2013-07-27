040e94df55115927d6de03c16dd365cd4d6edb2d
Huel Fuchsberger <huel.fuchsberger@gmx.de>
2013-07-27 18:00:06 +0200

    Added  Mouse Input

---
+++ Assets/Scripts/MouseInput.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+
+public class MouseInput : MonoBehaviour
+{
+    private GameObject dino;
+    private Vector3 mousePos;
+    private Vector3 worldPos; 
+
+    // Use this for initialization
+    void Start()
+    {
+        dino = GameObject.Find("DinoToken");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        const int orthographicSizeMin = 5;
+        const int orthographicSizeMax = 100;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Input.mousePosition;
+            worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Debug.Log("Dino Moved to " + worldPos);
+            dino.transform.position = new Vector3(worldPos.x, 1, worldPos.z);
+
+        }
+ 
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) // back
+        {
+            Camera.main.orthographicSize--;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) // forward
+        {
+            Camera.main.orthographicSize++;
+        }
+
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, orthographicSizeMin, orthographicSizeMax);
+    }
+}
